9c9fa251046fe78ca09cfaf41b6a02c3b5869db3
anderlr <anderlr@users.noreply.github.com>
2021-01-06 16:13:37 -0300

    Até a aula 16

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -4,7 +4,9 @@ import { Injectable } from '@angular/core'
 import { Http } from '@angular/http'
 import { Observable } from 'rxjs/Observable'
 import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/catch'
 
+import {ErrorHandler} from '../app.error-handler'
 @Injectable()
 export class RestaurantsService {
   constructor(private http: Http) { }
@@ -12,7 +14,12 @@ export class RestaurantsService {
   restaurants(): Observable<Restaurant[]> {
     return this.http.get(`${MEAT_API}/restaurants`)
       .map(response => response.json())//mapeia o observable recebido
+      .catch(ErrorHandler.handleError)
   }
 
-
+  restaurantById(id: string): Observable<Restaurant>{
+    return this.http.get(`${MEAT_API}/restaurants/${id}`)
+    .map(response => response.json())
+    .catch(ErrorHandler.handleError)
+  }
 }
