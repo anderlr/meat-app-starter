0c7892ae1cef6b98df069f03bd66dfd2def990e5
anderlr <anderlr@users.noreply.github.com>
2021-01-22 14:40:14 -0300

    Modularizando a aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
-import { HttpModule } from '@angular/http';
+import { HttpModule} from '@angular/http';
 import { RouterModule } from '@angular/router';
 
 import {ROUTES} from './app.routes'
@@ -8,7 +8,6 @@ import {ROUTES} from './app.routes'
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component'
 import {RestaurantsService} from './restaurants/restaurants.service';
@@ -20,19 +19,16 @@ import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
 import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
 import { OrderComponent } from './order/order.component';
 import {FormsModule,ReactiveFormsModule} from '@angular/forms';
-import { InputComponent } from './shared/input/input.component';
-import { RadioComponent } from './shared/radio/radio.component';
 import { OrderItemsComponent } from './order/order-items/order-items.component'
 import {OrderService} from './order/order.service';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
-import { RatingComponent } from './shared/rating/rating.component';
 import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component'
+import { SharedModule } from './shared/shared.module';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    AboutComponent,
     RestaurantsComponent,
     RestaurantComponent,
     RestaurantDetailComponent,
@@ -41,19 +37,16 @@ import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.co
     MenuItemComponent,
     ReviewsComponent,
     OrderComponent,
-    InputComponent,
-    RadioComponent,
     OrderItemsComponent,
     OrderSummaryComponent,
-    RatingComponent,
     DeliveryCostsComponent
   ],
   imports: [
     FormsModule,
     BrowserModule,
+    RouterModule.forRoot(ROUTES),
+    SharedModule,
     HttpModule,
-    ReactiveFormsModule,
-    RouterModule.forRoot(ROUTES)
   ],
   providers: [RestaurantsService,ShoppingCartService,OrderService,{provide: LOCALE_ID, useValue: 'pt-BR'}],
   bootstrap: [AppComponent]
